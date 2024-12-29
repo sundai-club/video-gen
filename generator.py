5ca9a3b25f0ce20fe03bd1d149e08f518ea88621
Nikolay Vyahhi <nikolay.vyahhi@hyperskill.org>
2024-12-29 16:09:46 -0500

    more robust video gen

--- generator.py
+++ generator.py
@@ -1,5 +1,6 @@
 import os
 import replicate
+from replicate.helpers import FileOutput
 
 from dotenv import load_dotenv
 from openai import OpenAI
@@ -12,9 +13,9 @@ def generate_video(script):
     """Generate a video using Replicate"""
     # Initialize the Replicate client
     client = replicate.Client(api_token=os.getenv("REPLICATE_API_TOKEN"))
-    # model, key = "minimax/video-01", "url" # output['url']
-    # model, key = "minimax/video-01-live", "url" # output['url'], but you need first_frame_image
-    model, key = "lightricks/ltx-video:c441c271f0cfd578aa0cd14a8488329dd10b796313a9335573a4a63507a976a5", 0  # output[0]
+    model = "minimax/video-01" # output['url']
+    # model = "minimax/video-01-live" # output['url'], but you need first_frame_image
+    # model = "lightricks/ltx-video:c441c271f0cfd578aa0cd14a8488329dd10b796313a9335573a4a63507a976a5" # output[0]
     output = client.run(model, input={"prompt": script, "prompt_optimizer": True})
     print(output)
     try:
@@ -27,7 +28,9 @@ def generate_video(script):
         output = output[0]
     if isinstance(output, dict):
         output = output['url']
-    return output[key]
+    if isinstance(output, FileOutput):
+        output = output.url
+    return output
 
 
 def generate_video_script(project_data):
@@ -36,7 +39,7 @@ def generate_video_script(project_data):
     client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
 
     # Prepare the messages
-    prompt = open("prompt-video-script.txt", "r").read()
+    prompt = open("prompt-video-script-005.txt", "r").read()
     prompt = prompt.format(**project_data)
     messages = [
         {"role": "user", "content": prompt},
